2e8a42d8dec06d83130887da628268d26810bbb0
realsteadyhands <xndrsmth@duck.com>
2025-07-30 23:53:08 -0500

    feat: created initial html and js files.

---
+++ index.js
@@ -0,0 +1,39 @@
+//? Are you using functions to organize your code?
+//? Do your variables and functions have meaningful names?
+//? Is your code formatted?
+
+const textInput = prompt("Enter a list of comma-separated froyo flavors");
+const description = 
+
+//! Prompt the user for froyo flavors and store their input
+
+
+
+//! Parse the user input into an array of froyo flavors
+
+
+
+//! Build an object to track which flavors you have counted so far
+
+
+
+//! Object to be updated through the array of flavors
+
+
+
+
+// ! TO DO
+
+// Student repo contains multiple commits with descriptive messages.
+// The repo contains an HTML file and a connected JS file.
+// The user is prompted for froyo flavors upon entering the website.
+// The user's input string is split into an array of strings.
+// A loop is used to iterate through the array of flavors.
+// An object is used to keep count of how many orders there are of each flavor.
+// The program correctly counts the number of each flavor in the user's input.
+// The logic for counting the frequencies of elements in an array is organized into a function that returns an object.
+// Variables and functions have meaningful names.
+// The console output changes depending on the user's input.
+
+
+*/
